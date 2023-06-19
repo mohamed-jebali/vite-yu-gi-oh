a8f2a2b68255f3c6e51a78bb37526f4f1a52de90
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-06-19 16:22:57 +0200

    added header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <h1>
-      Header
-    </h1>
+ <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand d-flex" href="#">
+      <img src="https://vignette1.wikia.nocookie.net/yugiohenespanol/images/a/aa/Yugioh_anime_logo.jpg/revision/latest?cb=20130310042737&path-prefix=es" alt="Logo" class="d-inline-block align-text-top">
+      <h2>
+        Yu-Gi-Oh Api
+      </h2>
+    </a>
   </div>
+</nav>
 </template>
 
 
@@ -19,5 +24,10 @@ export default {
  
 
 <style lang="scss">
+
+img{
+  height: 30px;
+  margin-right: 0.5rem;
+}
 
 </style>
